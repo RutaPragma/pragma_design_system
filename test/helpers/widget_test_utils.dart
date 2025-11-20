53a32313d008517c01a92b057f81aa8217351ef6
Jhony Renteria <jhonyrenteria@gmail.com>
2025-11-20 09:28:04 -0500

    Aumento de covertura de test

--- test/helpers/widget_test_utils.dart
+++ test/helpers/widget_test_utils.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
-import 'dart:async';
 import 'dart:convert';
-import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
